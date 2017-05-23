9a439e64a0ee8fcb37cc66c481f2764728062df1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2017-05-23 09:38:00 +0300

    unimportant

--- www/config.lua
+++
@@ -1,7 +0,0 @@
-config('basepath', '../www')
-config('lang', 'ro')
-config('smtp_host', '93.115.10.161')
-
-config('facebook_app_id', '725102964211663')
-config('google_client_id', '113821693132-an9cmghgm2fockigiubs1rp7tmfr9vnb.apps.googleusercontent.com')
-config('analytics_ua', 'UA-57284122-1')
